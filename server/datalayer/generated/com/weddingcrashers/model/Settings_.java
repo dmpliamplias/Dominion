e4ec84b594ffc8e84a4caaf5e71b7e3617043bb9
dmpliamplias <dyonisos.mpliamplias@students.fhnw.ch>
2017-09-29 11:18:16 +0200

    Add metamodel

---
+++ server/datalayer/generated/com/weddingcrashers/model/Settings_.java
@@ -0,0 +1,17 @@
+package com.weddingcrashers.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Settings.class)
+public abstract class Settings_ {
+
+	public static volatile SingularAttribute<Settings, String> settings;
+	public static volatile SingularAttribute<Settings, Boolean> sound;
+	public static volatile SingularAttribute<Settings, Long> id;
+	public static volatile SingularAttribute<Settings, Long> user;
+
+}
+
