7d447bad9c1727f720e1282b1b62e48f1aa23078
cajochen <32156907+cajochen@users.noreply.github.com>
2017-10-02 20:29:32 +0200

    Update ConnectionView.java

--- presentationlayer/src/main/java/connection/ConnectionView.java
+++ presentationlayer/src/main/java/connection/ConnectionView.java
@@ -16,12 +16,12 @@ public class ConnectionView extends View<ConnectionModel> {
         super(stage, model);
     }
 	
-	/**
+ /**
  *  @author Vanessa Cajochen
  *  */
 
     public Scene create_GUI(){
-        BorderPane root = new BorderPane();
+        	BorderPane root = new BorderPane();
 		Scene scene = new Scene(root,450,400);
 		GridPane gp = new GridPane();
 		FlowPane fp = new FlowPane();
@@ -62,10 +62,63 @@ public class ConnectionView extends View<ConnectionModel> {
 		stage.setTitle("Connection");
 		stage.setScene(scene);
 
-
         return scene;
     }
-
-
+    
+    public Scene create_Dialog(){
+		Stage secondStage = new Stage();
+		
+		secondStage.initOwner(stage);
+		secondStage.initModality(Modality.WINDOW_MODAL);					
+		
+		BorderPane root2 = new BorderPane();
+		Scene scene2 = new Scene(root2,400,220);
+		GridPane gp2 = new GridPane();
+		
+		// Creating and labeling button, label, textfield
+		Button btnConnect = new Button("Connect");			
+		Label lblPort = new Label("Port");
+	    	TextField fldPort = new TextField();
+		Label lblInfo = new Label("Insert port number");
+	    	    			
+		// Set size for the buttons and FlowPane
+		btnConnect.setPrefSize(80, 30);
+		lblPort.setPrefSize(50, 30);
+		fldPort.setPrefSize(150, 30);
+		
+		// Asign GridPane to BorderPane
+		root2.setCenter(gp2);
+				
+		// Creating columns with different sizes
+		ColumnConstraints column = new ColumnConstraints(50);
+		ColumnConstraints column1 = new ColumnConstraints(50);
+		ColumnConstraints column2 = new ColumnConstraints(150);
+		ColumnConstraints column3 = new ColumnConstraints(80);
+		ColumnConstraints column4 = new ColumnConstraints(120);
+		gp2.getColumnConstraints().addAll(column, column1, column2, column3, column4);
+							
+		// Creating 8 rows
+		for (int i = 0; i<8;i++){
+			RowConstraints row = new RowConstraints(30);
+			gp2.getRowConstraints().add(row);			
+		}
+		
+		// Asign buttons, label, textfield to column, row
+	  	gp2.setConstraints(lblInfo, 2, 1);
+	 	gp2.setConstraints(lblPort, 1, 3);
+	 	gp2.setConstraints(fldPort, 2, 3);
+	 	gp2.setConstraints(btnConnect, 3, 5);
+		gp2.getChildren().addAll(lblPort, fldPort, lblInfo, btnConnect);
+		
+		secondStage.setTitle("Insert Port");
+		secondStage.setScene(scene2);		
+    	    	
+        return scene2;
+    }
+       
+    
+    public Stage getStage(){
+        return stage;
+    }
 
 }
