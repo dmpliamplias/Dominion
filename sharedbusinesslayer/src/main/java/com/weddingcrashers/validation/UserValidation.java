14345192a30365551149ff855e5485b5288ab5b8
dmpliamplias <dyonisos.mpliamplias@students.fhnw.ch>
2017-10-02 12:50:50 +0200

    Add more validation

--- sharedbusinesslayer/src/main/java/com/weddingcrashers/validation/UserValidation.java
+++ sharedbusinesslayer/src/main/java/com/weddingcrashers/validation/UserValidation.java
@@ -3,8 +3,10 @@ package com.weddingcrashers.validation;
 import com.weddingcrashers.model.User;
 import com.weddingcrashers.service.UserService;
 import com.weddingcrashers.service.UserServiceImpl;
+import org.apache.commons.validator.routines.EmailValidator;
 
 import static com.weddingcrashers.validation.ValidationResult.EMAIL_ALREADY_EXISTS;
+import static com.weddingcrashers.validation.ValidationResult.EMAIL_IS_INVALID;
 import static com.weddingcrashers.validation.ValidationResult.OK;
 
 public class UserValidation {
@@ -40,6 +42,10 @@ public class UserValidation {
      * @return the validation result.
      */
     public ValidationResult validate() {
+        final boolean valid = EmailValidator.getInstance().isValid(email);
+        if (!valid) {
+            return EMAIL_IS_INVALID;
+        }
         final User userByEmail = userService.getUserByEmail(email);
         if (userByEmail != null) {
             validationResult = EMAIL_ALREADY_EXISTS;
--- sharedbusinesslayer/src/main/java/com/weddingcrashers/validation/ValidationResult.java
+++ sharedbusinesslayer/src/main/java/com/weddingcrashers/validation/ValidationResult.java
@@ -4,8 +4,9 @@ public enum ValidationResult {
 
     /** OK. */
     OK,
-
     /** E-mail already exists. */
-    EMAIL_ALREADY_EXISTS
+    EMAIL_ALREADY_EXISTS,
+    /** E-mail is invalid. */
+    EMAIL_IS_INVALID
 
 }
