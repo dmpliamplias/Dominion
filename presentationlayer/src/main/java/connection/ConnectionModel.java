4a57d05d8bc73c083d060fd281a949da32089d52
cajochen <32156907+cajochen@users.noreply.github.com>
2017-10-08 21:08:04 +0200

    Update ConnectionModel.java

--- presentationlayer/src/main/java/connection/ConnectionModel.java
+++ presentationlayer/src/main/java/connection/ConnectionModel.java
@@ -3,9 +3,29 @@ package connection;
 import base.Model;
 
 /**
- *  @author Michel Schlatter
+ *  @author  Michel Schlatter & Vanessa Cajochen
  *  */
 public class ConnectionModel extends Model {
+    private String Url;
+    private int Port;
+    private String IP;
+    
+    public String getIP() {
+        return IP;
+    }
+    
+    public void setIP(String ip){
+        IP = ip;
+    }
+    
+    public int getPort() {
+        return Port;
+    }
+    
+    public void setPort(int port){
+        Port = port;
+    }
+         
     public String getUrl() {
         return Url;
     }
@@ -14,7 +34,6 @@ public class ConnectionModel extends Model {
         Url = url;
     }
 
-    private String Url;
-    private int Port;
+
 
 }
