66d38379cdfa5a7fc4ff0a11fd91bea66ade675d
Michel Schlatter <michel.schlatter@gmx.net>
2017-10-12 17:58:36 +0200

    bug fixes chat

--- presentationlayer/src/main/java/Game/GameController.java
+++ presentationlayer/src/main/java/Game/GameController.java
@@ -10,17 +10,17 @@ import com.weddingcrashers.service.Translator;
 import javafx.application.Platform;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Separator;
+import javafx.scene.paint.Color;
 
 import java.io.IOException;
 
 
 public class GameController extends Controller<GameModel, GameView> {
-    private final ServerConnectionService serverConnection;
 
 
     public GameController(GameModel model, GameView view, boolean myTurn){
         super(model,view);
-        serverConnection = PLServiceLocator.getPLServiceLocator().getServerConnectionService();
+        serverConnectionService.setGameController(this);
         initialize();
 
     }
@@ -28,7 +28,7 @@ public class GameController extends Controller<GameModel, GameView> {
 
     public void initialize() {
         try {
-            serverConnection.updateViewStatus(ViewStatus.Game); // set ViewStatus for Server
+            serverConnectionService.updateViewStatus(ViewStatus.Game); // set ViewStatus for Server
 
         } catch (IOException e) {
             this.view.alert(e.getMessage(), Alert.AlertType.ERROR);
@@ -58,7 +58,7 @@ public class GameController extends Controller<GameModel, GameView> {
             cc.setMsg(message );
 
             try {
-                serverConnection.sendObject(cc);
+                serverConnectionService.sendObject(cc);
             } catch (IOException e) {
                 view.alert(e.getMessage(), Alert.AlertType.ERROR);
             }
@@ -69,9 +69,14 @@ public class GameController extends Controller<GameModel, GameView> {
 
             ChatContainer cc = new ChatContainer();
             cc.setMsg( view.btnSendText.getText());
+// TODO: 12.10.2017  Manu => wenn du schribsch den trägs au grad ih, ich shicks nur de andere clients, nöd dir auno (wills du ja de sender bish
+        // und weisch was d schribsch. + add no de name vom client über 'plServiceLocator.getUser().getUserName()'
+        //denn no farbig über d id vom client 'serverConnectionService.getClientId()'
+        // es sind max 4 clients...den chash ja für 1 = blau für 2 = gelb usw.....
+
 
             try {
-                serverConnection.sendObject( cc );
+                serverConnectionService.sendObject( cc );
             } catch (IOException e) {
                 view.alert(e.getMessage(), Alert.AlertType.ERROR);
             }
@@ -84,12 +89,41 @@ public class GameController extends Controller<GameModel, GameView> {
 
             System.out.println("Empfangen");
             String beforeText = view.textAreaChat.getText();
-            String newText = beforeText += System.getProperty("line.separator") + chatContainer.getMsg();
+            String newText = beforeText += System.getProperty("line.separator") + CreateChatText(chatContainer);
             view.textAreaChat.setText(newText);
 
         });
 
     }
 
+    private String CreateChatText(ChatContainer chatContainer){
+        Color c = getColorByClientId(chatContainer);
+        // TODO: 12.10.2017 lock here how to set color https://stackoverflow.com/questions/30114478/is-it-possible-to-set-different-colors-for-different-lines-in-a-javafx-textfield
+        // you hava to replace Textares with InlineCssTextArea or HTMLEDITOR ==> google helps
 
+        String msg = plServiceLocator.getUser().getUserName() + " :" + chatContainer.getMsg();
+        return msg;
+    }
+
+    private Color getColorByClientId(ChatContainer chatContainer){
+       int id = chatContainer.getClientId();
+       Color c = Color.WHITE;
+
+       switch(id) {
+           case 1:
+               c = Color.BLUE;
+               break;
+           case 2:
+               c = Color.YELLOW;
+               break;
+           case 3:
+               c = Color.GREEN;
+               break;
+           case 4:
+               c = Color.RED;
+               break;
+       }
+
+       return c;
+    }
 }
