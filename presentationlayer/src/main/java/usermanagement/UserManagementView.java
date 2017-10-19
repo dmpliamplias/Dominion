ab5fcb30a9f20261f0ea5561bcb82312f180ca1d
Dyonisos Mpliamplias <dyonisos.mpliamplias@inventage.com>
2017-10-19 11:58:08 +0200

    Add initial layout

--- presentationlayer/src/main/java/usermanagement/UserManagementView.java
+++ presentationlayer/src/main/java/usermanagement/UserManagementView.java
@@ -4,6 +4,7 @@ import base.View;
 import com.weddingcrashers.model.User;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -11,7 +12,11 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import static javafx.geometry.Pos.TOP_RIGHT;
+import static javafx.scene.text.FontWeight.NORMAL;
 
 /**
  * The user management view.
@@ -21,12 +26,6 @@ import javafx.stage.Stage;
 public class UserManagementView extends View<UserManagementModel> {
 
     // ---- Members
-
-    /** The list view. */
-    private ListView<User> listView;
-
-    /** The title label. */
-    private Label title;
 
     /** The name field. */
     private TextField name;
@@ -39,8 +38,6 @@ public class UserManagementView extends View<UserManagementModel> {
     /** The is super user checkbox. */
     private CheckBox isSuperUser;
 
-    /** The edit button. */
-    private Button editButton;
     /** The confirm button. */
     private Button confirmButton;
     /** The cancel button. */
@@ -72,27 +69,42 @@ public class UserManagementView extends View<UserManagementModel> {
 
         // layout
         final VBox root = new VBox();
+        root.setSpacing(15);
+        root.setPadding(new Insets(20));
 
         // initialize fields
-        listView = new ListView<>(users);
-        title = new Label();
         name = new TextField();
         email = new TextField();
         password = new TextField();
         isBlocked = new CheckBox();
         isSuperUser = new CheckBox();
-        editButton = new Button();
         confirmButton = new Button();
         cancelButton = new Button();
 
-        title.setText("User Management Dominion");
-        editButton.setText("Edit User");
-        root.getChildren().addAll(title, listView, editButton);
+        root.getChildren().addAll(addTitle(), addListView(), addEditButton());
 
         final Scene scene = new Scene(root);
         stage.setScene(scene);
 
         return scene;
+    }
+
+    private Label addTitle() {
+        final Label title = new Label();
+        title.setText("User Management Dominion");
+        title.setFont(Font.font("Arial", NORMAL, 35));
+        return title;
+    }
+
+    private ListView<User> addListView() {
+        return new ListView<>(users);
+    }
+
+    private Button addEditButton() {
+        final Button editButton = new Button();
+        editButton.setAlignment(TOP_RIGHT);
+        editButton.setText("Edit User");
+        return editButton;
     }
 
     @Override
