46446bfa08bc1adfea2392e914c38e97ba4a3e61
“Manuel <manuel_wirz@hotmail.de>
2017-10-24 18:09:39 +0200

    todo murat...

--- presentationlayer/src/main/java/lobby/LobbyController.java
+++ presentationlayer/src/main/java/lobby/LobbyController.java
@@ -79,6 +79,7 @@ public class LobbyController extends Controller <LobbyModel, LobbyView> {
         } );
 
         //author Murat Kelleci am 24.10.2017
+        //TODO fix bug murat
         view.btnRanking.setOnAction(event -> {
             goToRankingView();
         });
