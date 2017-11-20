cf0ee3e99ffd14980792ac33358cd6653eea948d
muratkelleci <murat.kelleci@students.fhnw.ch>
2017-11-20 19:05:20 +0100

    add tableview to rankingview

--- presentationlayer/src/main/java/ranking/RankingView.java
+++ presentationlayer/src/main/java/ranking/RankingView.java
@@ -2,8 +2,6 @@ package ranking;
 
 import base.View;
 import com.weddingcrashers.model.Highscore;
-import com.weddingcrashers.model.User;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
@@ -15,8 +13,6 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import java.util.ArrayList;
-import java.util.List;
 
 
 //** @author Murat Kelleci - Credits:http://docs.oracle.com/javafx/2/ui_controls/table-view.htm
@@ -26,9 +22,9 @@ public class RankingView extends View<RankingModel> {
     TableView<RankingViewModel> table;
 
     protected Label lblRanking;
-    protected TableColumn userNameColumn;
-    protected TableColumn userHighScoreColumn;
-    protected TableColumn rankingPositionColumn;
+    protected TableColumn<RankingViewModel, String> userNameColumn;
+    protected TableColumn<RankingViewModel, String> userHighScoreColumn;
+    protected TableColumn<RankingViewModel, String> rankingPositionColumn;
     protected Button btnLobby;
 
     public RankingView(Stage stage, RankingModel model) {
@@ -67,9 +63,9 @@ public class RankingView extends View<RankingModel> {
 
     protected void setTexts() {
 
-        this.userNameColumn = new TableColumn("Name");
-        this.rankingPositionColumn = new TableColumn(("Position"));
-        this.userHighScoreColumn = new TableColumn("Points");
+        this.userNameColumn = new TableColumn<>("Name");
+        this.rankingPositionColumn = new TableColumn<RankingViewModel, String>(("Position"));
+        this.userHighScoreColumn = new TableColumn<RankingViewModel, String>("Points");
 
         this.table.getColumns().clear();
         this.table.getColumns().addAll(rankingPositionColumn, userNameColumn, userHighScoreColumn);
@@ -85,11 +81,10 @@ public class RankingView extends View<RankingModel> {
     public void bindModelToView() {
 
         final ObservableList<RankingViewModel> vmList = FXCollections.observableArrayList();
-        RankingViewModel vm2 = new RankingViewModel();
-        vm2.setName("Murat Kelleci");
-        vm2.setPoints(1);
-        vm2.setPosition(2);
-        vmList.add(vm2);
+       // RankingViewModel vm2 = new RankingViewModel();
+      // vm2.setName("Murat Kelleci");vm2.setPoints(1);
+       //vm2.setPosition(2);
+       //vmList.add(vm2);
 
         //For Loop from Script Kaspar Riesen Programming 1
         for (Highscore hs : model.getHighscores()) {
