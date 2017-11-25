4ff279919552e5e890f6e23f8e06a82010438ab1
Vanessa Cajochen <vanessa.cajochen@gmail.com>
2017-11-25 16:21:33 +0100

    PlayerSet -> added Methods add and remove

--- sharedbusinesslayer/src/main/java/com/weddingcrashers/businessmodels/PlayerSet.java
+++ sharedbusinesslayer/src/main/java/com/weddingcrashers/businessmodels/PlayerSet.java
@@ -132,4 +132,35 @@ public class PlayerSet implements Serializable {
         this.handStack = handStack;
     }
 
+
+
+    /**
+     * @author  Vanessa Cajochen
+     */
+
+    public void addCardToHandStack (Card card){
+        handStack.add(card);
+    }
+
+    public void removeCardFromHandStack (Card card){
+        handStack.remove(card);
+    }
+
+    public void addCardToPullStack (Card card){
+        pullStack.add(card);
+    }
+
+    public void removeCardFromPullStack (Card card){
+        pullStack.remove(card);
+    }
+
+    public void addCardToTrayStack (Card card){
+        trayStack.add(card);
+    }
+
+    public void removeCardFromTrayStack (Card card){
+        trayStack.remove(card);
+    }
+
+
 }
