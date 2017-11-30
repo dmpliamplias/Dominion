dd99a94487848cb8709b9cdc00e7a129d9e59fb0
muratkelleci <murat.kelleci@students.fhnw.ch>
2017-11-30 16:04:08 +0100

    added Methode showPointsAndPlayer

--- presentationlayer/src/main/java/Game/GameController.java
+++ presentationlayer/src/main/java/Game/GameController.java
@@ -501,7 +501,7 @@ public class GameController extends Controller<GameModel, GameView> {
         String userName = users.get(userId).getUserName();
 
         this.view.setUserPoints(userId, userName, playerSet);
-        
+
     }
 
 
