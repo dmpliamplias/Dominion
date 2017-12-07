1643604f02dff56fa459e6db5f16ad60714bf4dc
“Manuel <manuel_wirz@hotmail.de>
2017-12-07 18:14:33 +0100

    lobby bug

--- presentationlayer/src/main/java/lobby/LobbyController.java
+++ presentationlayer/src/main/java/lobby/LobbyController.java
@@ -234,6 +234,10 @@ public class LobbyController extends Controller <LobbyModel, LobbyView> {
         if (!view.cbFinishPointCards.isSelected() && view.choiceBox.getSelectionModel().isEmpty()) {
             view.alert( getText( "lobbyview.falseStatement" ), Alert.AlertType.WARNING );
 
+        } else if (view.cbFinishPointCards.isSelected() && !view.choiceBox.getSelectionModel().isEmpty()) {
+            view.alert( getText( "lobbyview.falseStatement" ), Alert.AlertType.WARNING );
+            view.choiceBox.getSelectionModel().clearSelection();
+            view.cbFinishPointCards.setSelected( false );
         } else if (players.size() >= 2 && view.lvPlayers.getSelectionModel().getSelectedItems().size() <= 1) {
 
         view.startStage().show();
@@ -249,10 +253,6 @@ public class LobbyController extends Controller <LobbyModel, LobbyView> {
 
         } else if (names.size() < 2) {
             view.alert( getText( "lobbyview.notEnoughPlayers" ), Alert.AlertType.WARNING );
-        } else if (view.cbFinishPointCards.isSelected() && !view.choiceBox.getSelectionModel().isEmpty()) {
-            view.alert( getText( "lobbyview.falseStatement" ), Alert.AlertType.WARNING );
-            view.choiceBox.getSelectionModel().clearSelection();
-            view.cbFinishPointCards.setSelected( false );
         } else if(!view.lvPlayers.getSelectionModel().isSelected( 0 )) {
             view.alert( getText( "lobbyview.serverIsNotSelected" ), Alert.AlertType.ERROR );
         } else {
