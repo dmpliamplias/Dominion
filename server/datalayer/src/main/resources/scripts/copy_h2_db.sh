2eeda9d91efb79b0e0dd93c3d3447d3850e33abb
Dyonisos Mpliamplias <dyonisos.mpliamplias@inventage.com>
2017-12-19 14:33:12 +0100

    Remove rm call

--- server/datalayer/src/main/resources/scripts/copy_h2_db.sh
+++ server/datalayer/src/main/resources/scripts/copy_h2_db.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo 'Start copying database...'
-rm ~/.h2/weddingcrashers_dominion.mv.db
+#rm ~/.h2/weddingcrashers_dominion.mv.db
 cp ../cleanDB/weddingcrashers_dominion.mv.db ~/.h2/
 echo 'Copied database'
